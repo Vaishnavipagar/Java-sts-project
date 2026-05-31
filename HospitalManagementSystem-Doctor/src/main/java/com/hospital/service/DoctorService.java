d791d230871ca9db49f843582eda669f967ce4cb
Vaishnavipagar <vaishanvipagar7@gmail.com>
2026-05-31 18:29:48 +0530

    chneg

---
+++ HospitalManagementSystem-Doctor/src/main/java/com/hospital/service/DoctorService.java
@@ -0,0 +1,34 @@
+package com.hospital.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.hospital.entity.Doctor;
+import com.hospital.repository.DoctorRepository;
+@Service
+public class DoctorService {
+	@Autowired
+	private DoctorRepository doctorRepository;
+	
+	public List<Doctor> getAllDoctors()
+	{
+		return doctorRepository.findAll();
+	}
+	
+	public Doctor saveDoctor(Doctor doctor)
+	{
+		return doctorRepository.save(doctor);
+	}
+	
+	public List<Doctor> getAllDocBySpec(String spec){
+		return doctorRepository.getAllDoctorBySpec(spec);
+	}
+	
+	public Doctor getDocBySpec(String spec)
+	{
+		return doctorRepository.getDistinctDoctorBySpec(spec);
+	}
+
+}
